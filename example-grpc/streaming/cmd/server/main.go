ba7bc07b944902a4e6e3f3194024705f13ff4217
Chris Shepherd <chris_shepherd2@hotmail.com>
2024-01-10 23:18:35 +0000

    add streaming examples

---
+++ example-grpc/streaming/cmd/server/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"net"
+	"os"
+
+	"golang.org/x/sync/errgroup"
+	"google.golang.org/grpc"
+
+	"github.com/cshep4/proto-repository/example-grpc/streaming/proto"
+)
+
+func main() {
+	ctx := context.Background()
+
+	gs := grpc.NewServer()
+
+	helloService := &stream.Service{}
+
+	proto.RegisterHelloServiceServer(gs, helloService)
+
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		slog.Error("failed to listen", slog.String("error", err.Error()))
+		os.Exit(1)
+	}
+
+	eg, ctx := errgroup.WithContext(ctx)
+
+	eg.Go(func() error {
+		slog.Info("starting grpc server", slog.String("address", ":50051"))
+		return gs.Serve(lis)
+	})
+
+	if err := eg.Wait(); err != nil {
+		slog.Error("error running server", slog.String("error", err.Error()))
+		os.Exit(1)
+	}
+}
---
+++ example-grpc/streaming/internal/stream/service.go
@@ -0,0 +1,64 @@
+package stream
+
+import (
+	"fmt"
+	"github.com/cshep4/proto-repository/example-grpc/streaming/proto"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"io"
+)
+
+type Service struct {
+	*proto.UnimplementedStreamingServiceServer
+}
+
+func (s Service) ServerStream(req *proto.ServerStreamRequest, stream proto.StreamingService_ServerStreamServer) error {
+	if req.GetMessage() == "" {
+		return status.Error(codes.InvalidArgument, "name cannot be empty")
+	}
+
+	for i := 0; i < 5; i++ {
+		res := &proto.ServerStreamResponse{
+			Message: fmt.Sprintf("%d) %s", i, req.GetMessage()),
+		}
+		if err := stream.Send(res); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (s Service) ClientStream(stream proto.StreamingService_ClientStreamServer) error {
+	var receivedMessages []string
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(&proto.ClientStreamResponse{ReceivedMessages: receivedMessages})
+		}
+		if err != nil {
+			return err
+		}
+
+		receivedMessages = append(receivedMessages, req.Name)
+	}
+}
+
+func (s Service) BiDirectionalStream(stream proto.StreamingService_BiDirectionalStreamServer) error {
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+
+		res := &proto.BiDirectionalStreamResponse{
+			Message: fmt.Sprintf("Hello %s!", req.GetName()),
+		}
+		if err := stream.Send(res); err != nil {
+			return err
+		}
+	}
+}
